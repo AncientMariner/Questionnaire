5830f86735a62263028edd9d3ad0be3862f2e669
AncientMariner <xander.lysak@gmail.com>
2014-05-05 14:20:31 +0300

    removed unnecessary comments

--- src/main/java/org/xander/questionnaire/dao/ContentHibernateDao.java
+++ src/main/java/org/xander/questionnaire/dao/ContentHibernateDao.java
@@ -3,6 +3,4 @@ package org.xander.questionnaire.dao;
 import org.xander.questionnaire.model.Content;
 
 public class ContentHibernateDao extends AbstractHibernateDao<Content> implements Dao<Content> {
-    public ContentHibernateDao() {
-    }
 }
